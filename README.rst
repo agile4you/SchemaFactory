3771e8136c99e6f5c74e8da3ac91f8cb6ef13548
Vassilis Papavassiliou <pav@geotagaeroview.com>
2016-01-14 17:13:40 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,1 @@
+**SchemaDoctor**:  *python schema made easy...*
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='SchemaFactory',
+    version='1.0',
+    packages=['test'],
+    url='https://github.com/agile4you/SchemaFactory',
+    license='GLPv3',
+    author='Papavassiliou Vassilis',
+    author_email='vpapavasil@gmail.com',
+    description='Python Schema toolkit'
+)
