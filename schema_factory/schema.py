a06aac29c36330cd2d1c8208ad9a6b8c54355702
PyB1l <vpapavasil@gmail.com>
2017-02-16 16:45:39 +0200

    Fixed forgotten print statements!

--- schema_factory/schema.py
+++ schema_factory/schema.py
@@ -24,9 +24,6 @@ class SchemaType(type):
 
     def __new__(mcs, name, bases, attrs):
 
-        print(bases)
-        print(attrs.keys())
-
         schema_nodes = {k: v for k, v in attrs.items() if
                         isinstance(v, BaseNode)}
 
